53a1cf41cca4559c93c06ffde389445e4a866ff3
lujialei <lujialei08@163.com>
2024-06-17 22:23:59 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,147 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://haikelei.dev/", // replace this with your deployed domain
+  author: "Gary Lu",
+  desc: "Gary Lu's personal blog, sharing front-end development, web design, and other interesting things",
+  title: "haikelei的博客",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: false,
+  postPerPage: 6,
+  scheduledPostMargin: 15 * 60 * 1000
+};
+
+export const LOCALE = {
+  lang: "zh",
+  langTag: ["zh-CN"],
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/haikelei/haikelei-blog",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/gary-lu08/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "lujialei08@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/GaryOhNoooooo",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,27 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+* 我是一名前端工程师，我爱前端开发。
+* 瞎折腾，尝试新事物，学习新知识。
+* 不喜欢网红技术环境，不喜欢讨论哪种编程语言和框架好。
+* 追求长期主义，追求简单，追求效率。
+
+<div>
+  <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
+
+## Tech Stack
+
+* 前端：Vue, React, TypeScript, TailwindCSS, Vite
+* 后端：Java, Spring Boot, MySQL, Redis
+* 运维：Docker, Nginx, CI/CD流水线
+* 设计：Figma
+
+## Contact
+
+- **微信**: haikelei
+- **邮箱**: lujialei08@gmail.com
+- **Github**: [链接](https://github.com/haikelei)
+- **LinkedIn**: [链接](https://www.linkedin.com/in/gary-lu08/)
